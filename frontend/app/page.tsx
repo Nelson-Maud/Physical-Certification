b1f68323812ae7f13e4baea027b8197a96b77991
Nelson-Maud <lvelinda895@gmail.com>
2025-11-10 11:45:53 +0000

    add component documentation

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,6 @@
 import { PhysicalCertificationDemo } from "@/components/PhysicalCertificationDemo";
 
+// Main page component for the Physical Certification application
 export default function Home() {
   return (
     <main className="w-full">
